54409c5b47115dbd23d7996f9b715360901e6602
Кирилл Шацкий <kshatskiy@Kirills-iMac.local>
2017-12-27 11:18:23 +0300

    Add createUser test

--- test/mutation.js
+++ test/mutation.js
@@ -1,6 +1,33 @@
+const {expect} = require('chai');
+
+const {start, stop, runQuery} = require('../server');
+
 describe('Mutation', () => {
   describe('#createUser', () => {
+    let server;
+
+    before(() => {
+      return start().then((instance) => {
+        server = instance;
+      });
+    });
+    
+    after(() => {
+      return stop(server);
+    });
+
     it('should create a user', () => {
+      return runQuery(server, `mutation {
+        createUser(input: {login: "testUser"}) {
+          login
+        }
+      }`).then(({body: {data, errors}}) => {
+        expect(data).to.eql({
+            createUser: {
+              login: "testUser"
+            }
+        });
+      })
     });
   });
 });
