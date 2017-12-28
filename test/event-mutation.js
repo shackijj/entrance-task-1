2442bc0a41c1cb71eb0537c12a2c88e2ef297ebd
Kirill Shatskiy <kshatskiy@Kirills-Mini.iponweb.lan>
2017-12-28 11:37:57 +0300

    Add event-mutation boilerplate

---
+++ test/event-mutation.js
@@ -0,0 +1,18 @@
+const {start, stop, clearDatabase} = require('../src/server')
+
+describe('Event mutations', () => {
+  let server
+
+  before(() => {
+    return start()
+      .then((instace) => {
+        server = instace
+        return instace
+      })
+      .then(clearDatabase)
+  })
+
+  after(() => {
+    return stop(server)
+  })
+})
