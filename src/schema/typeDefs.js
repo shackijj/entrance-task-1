21325ba47cc6e9b381b04cab1cbcf91813d11192
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-03 09:33:05 +0300

    Add tests for room and event queries

--- src/schema/typeDefs.js
+++ src/schema/typeDefs.js
@@ -59,7 +59,7 @@ type Event {
     title: String!
     dateStart: Date!
     dateEnd: Date!
-    users: [User]
+    users: [User!]
     room: Room
 }
 
@@ -90,7 +90,7 @@ input ChangeEventRoomInput {
 
 type Query {
   user(id: ID!): User
-  users: [User]
+  users: [User!]
   event(id: ID!): Event
   events: [Event]
   room(id: ID!): Room
