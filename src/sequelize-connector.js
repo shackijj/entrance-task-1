ec77f8d380c1c9f8f76a0057be90dc8bf923231a
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-14 08:29:42 +0300

    Update db path

--- src/sequelize-connector.js
+++ src/sequelize-connector.js
@@ -8,7 +8,7 @@ const DB_PASSWORD = 'password'
 module.exports = () => {
   const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     dialect: 'sqlite',
-    storage: '../db.sqlite3',
+    storage: './db.sqlite3',
     operatorsAliases: { $and: Sequelize.Op.and },
     logging: false
   })
